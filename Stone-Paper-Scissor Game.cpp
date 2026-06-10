9f2a3138c34e62d61d56de1c843be9826c365091
Ziyad Albokhari <ziyad.y.albokhari@gmail.com>
2026-06-10 20:04:54 +0300

    Update Stone-Paper-Scissor Game.cpp

--- Stone-Paper-Scissor Game.cpp
+++ Stone-Paper-Scissor Game.cpp
@@ -1,261 +1,578 @@
 #include <iostream>
-#include <ctime>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
 #include <cstdlib>
 
 using namespace std;
 
-enum enGameChoice { Stone = 1, Paper = 2, Scissors = 3 };
-enum enWinner { Player1 = 1, Computer = 2, Draw = 3 };
+const string ClientsFileName = "Clients.txt";
 
-struct stRoundInfo
+void ShowMainMenue();
+
+struct stClient
 {
-	short RoundNumber = 0;
-	enGameChoice Player1Choice;
-	enGameChoice Computer1Choice;
-	enWinner Winner;
-	string WinnerName = "";
+	string AccountNumber = "";
+	string PinCode = "";
+	string Name = "";
+	string Phone = "";
+	float AccountBalance = 0;
+	bool MarkForDelete = false;
 };
 
-struct stGameResults
+vector <string> SplitString(string S1, string Delim)
 {
-	short GameRounds = 0;
-	short Player1WonTimes = 0;
-	short ComputerWonTimes = 0;
-	short DrawTimes = 0;
-	enWinner GameWinner;
-	string WinnerName = "";
-};
+	vector <string> vString;
 
-int RandomNumber(int From, int To)
-{
-	int RandNum = rand() % (To - From + 1) + From;
+	string sWord = "";
+	int pos = 0;
 
-	return RandNum;
-}
-
-string WinnerName(enWinner Winner)
-{
-	string arrWinnerName[3] = { "Player1", "Computer", "No Winner" };
-
-	return arrWinnerName[Winner - 1];
-}
-
-enWinner HowWonTheRound(stRoundInfo RoundInfo)
-{
-	if (RoundInfo.Player1Choice == RoundInfo.Computer1Choice)
+	while ((pos = S1.find(Delim)) != string::npos)
 	{
-		return enWinner::Draw;
+		sWord = S1.substr(0, pos);
+
+		if (sWord != "")
+		{
+			vString.push_back(sWord);
+		}
+
+		S1.erase(0, pos + Delim.length());
 	}
 
-	switch (RoundInfo.Player1Choice)
+	if (S1 != "")
 	{
-	case enGameChoice::Stone:
-		if (RoundInfo.Computer1Choice == enGameChoice::Paper)
-		{
-			return enWinner::Computer;
-		}
-		break;
-
-	case enGameChoice::Paper:
-		if (RoundInfo.Computer1Choice == enGameChoice::Scissors)
-		{
-			return enWinner::Computer;
-		}
-		break;
-
-	case enGameChoice::Scissors:
-		if (RoundInfo.Computer1Choice == enGameChoice::Stone)
-		{
-			return enWinner::Computer;
-		}
-		break;
+		vString.push_back(S1);
 	}
 
-	return enWinner::Player1;
+	return vString;
 }
 
-string ChoiceName(enGameChoice Choice)
+stClient ConvertLineRecord(string Line, string Seperator = "#//#")
 {
-	string arrNameChoice[3] = { "Stone", "Paper", "Scissors" };
+	stClient Client;
+	vector <string> vClientData;
 
-	return arrNameChoice[Choice - 1];
+	vClientData = SplitString(Line, Seperator);
+
+	Client.AccountNumber = vClientData[0];
+	Client.PinCode = vClientData[1];
+	Client.Name = vClientData[2];
+	Client.Phone = vClientData[3];
+	Client.AccountBalance = stof(vClientData[4]);
+
+	return Client;
 }
 
-void SetWinnerScreenColor(enWinner Winner)
+string ConvertRecordToLine(stClient Client, string Seperator = "#//#")
 {
-	switch (Winner)
+	string stClientRecord = "";
+
+	stClientRecord += Client.AccountNumber + Seperator;
+	stClientRecord += Client.PinCode + Seperator;
+	stClientRecord += Client.Name + Seperator;
+	stClientRecord += Client.Phone + Seperator;
+	stClientRecord += to_string(Client.AccountBalance);
+
+	return stClientRecord;
+}
+
+vector <stClient> LoadCleintsDataFromFile(string FileName)
+{
+	vector <stClient> vClients;
+
+	fstream MyFile;
+
+	MyFile.open(FileName, ios::in);
+
+	if (MyFile.is_open())
 	{
-	case enWinner::Player1:
-		system("color 2F"); //Green
-		break;
+		string Line;
+		stClient Client;
 
-	case enWinner::Computer:
-		system("color 4F"); //Red
-		cout << "\a";
-		break;
+		while (getline(MyFile, Line))
+		{
+			Client = ConvertLineRecord(Line);
 
-	default:
-		system("color 6F"); //Yellow
-		break;
+			vClients.push_back(Client);
+		}
+
+		MyFile.close();
+	}
+
+	return vClients;
+}
+
+void AddDataLineToFile(string FileName, string stDataLine)
+{
+	fstream MyFile;
+
+	MyFile.open(FileName, ios::out | ios::app);
+
+	if (MyFile.is_open())
+	{
+		MyFile << stDataLine << endl;
+
+		MyFile.close();
 	}
 }
 
-void PrintRoundResults(stRoundInfo RoundInfo)
+vector <stClient> SaveCleintsDataToFile(string FileName, vector <stClient> vClients)
 {
-	cout << "\n____________Round [" << RoundInfo.RoundNumber << "] ____________\n\n";
-	cout << "Player1  Choice: " << ChoiceName(RoundInfo.Player1Choice) << endl;
-	cout << "Computer Choice: " << ChoiceName(RoundInfo.Computer1Choice) << endl;
-	cout << "Round Winner   : [" << RoundInfo.WinnerName << "]\n";
-	cout << "__________________________________\n" << endl;
+	fstream MyFile;
 
-	SetWinnerScreenColor(RoundInfo.Winner);
+	MyFile.open(FileName, ios::out);
+
+	if (MyFile.is_open())
+	{
+		string ClientLine;
+
+		for (stClient& C : vClients)
+		{
+			if (C.MarkForDelete == false)
+			{
+				ClientLine = ConvertRecordToLine(C);
+
+				MyFile << ClientLine << endl;
+			}
+		}
+
+		MyFile.close();
+	}
+
+	return vClients;
 }
 
-enWinner HowWonTheGame(short Player1WinTimes, short ComputerWinTimes)
+vector <stClient> SaveCleintsDataToFile(string FileName, vector <stClient> vClients, string AccountNumber, stClient Client)
 {
-	if (Player1WinTimes > ComputerWinTimes)
-		return enWinner::Player1;
-	else if (ComputerWinTimes > Player1WinTimes)
-		return enWinner::Computer;
+	fstream MyFile;
+
+	MyFile.open(FileName, ios::out);
+
+	if (MyFile.is_open())
+	{
+		string ClientLine;
+
+		for (stClient& C : vClients)
+		{
+			ClientLine = ConvertRecordToLine(C);
+
+			MyFile << ClientLine << endl;
+		}
+
+		MyFile.close();
+	}
+
+	return vClients;
+}
+
+void PrintClientCard(stClient Client)
+{
+	cout << "\nThe following are the client details:\n";
+	cout << "-----------------------------------\n";
+	cout << "Account Number : " << Client.AccountNumber << endl;
+	cout << "Pin Code       : " << Client.PinCode << endl;
+	cout << "Name           : " << Client.Name << endl;
+	cout << "Phone          : " << Client.Phone << endl;
+	cout << "Account Balance: " << Client.AccountBalance << endl;
+	cout << "-----------------------------------\n";
+}
+
+void PrintClientRecord(stClient Client)
+{
+	cout << "| " << left << setw(15) << Client.AccountNumber;
+	cout << "| " << left << setw(10) << Client.PinCode;
+	cout << "| " << left << setw(40) << Client.Name;
+	cout << "| " << left << setw(12) << Client.Phone;
+	cout << "| " << left << setw(12) << Client.AccountBalance;
+}
+
+bool ClientExistsByAccountNumber(string AccountNumber, string FileName)
+{
+	vector <stClient> vClients;
+	fstream MyFile;
+
+	MyFile.open(FileName, ios::in);
+
+	if (MyFile.is_open())
+	{
+		string Line;
+		stClient Client;
+
+		while (getline(MyFile, Line))
+		{
+			Client = ConvertLineRecord(Line);
+
+			if (Client.AccountNumber == AccountNumber)
+			{
+				MyFile.close();
+
+				return true;
+			}
+
+			vClients.push_back(Client);
+		}
+
+		MyFile.close();
+	}
+
+
+
+	return false;
+}
+
+stClient ReadNewClient()
+{
+	stClient Client;
+
+	cout << "Enter Account Number? ";
+	getline(cin >> ws, Client.AccountNumber);
+
+	while (ClientExistsByAccountNumber(Client.AccountNumber, ClientsFileName))
+	{
+		cout << "Client white [" << Client.AccountNumber << "] already exists, Enter another Account Number? ";
+		getline(cin >> ws, Client.AccountNumber);
+	}
+
+	cout << "Enter Pin Code? ";
+	getline(cin, Client.PinCode);
+
+	cout << "Enter Name? ";
+	getline(cin, Client.Name);
+
+	cout << "Enter Phone? ";
+	getline(cin, Client.Phone);
+
+	cout << "Enter Account Balance? ";
+	cin >> Client.AccountBalance;
+
+	return Client;
+}
+
+void AddNewClient()
+{
+	stClient Client = ReadNewClient();
+	AddDataLineToFile(ClientsFileName, ConvertRecordToLine(Client));
+}
+
+void AddClients()
+{
+	char AddMore = 'Y';
+
+	do
+	{
+		cout << "\nAdding New Client:\n\n";
+		AddNewClient();
+
+		cout << "\nClient Added Successfully, do you want to add more clients? Y/N? ";
+		cin >> AddMore;
+
+	} while (toupper(AddMore) == 'Y');
+}
+
+string ReadClientAccountNumber()
+{
+	string AccountNumber = "";
+
+	cout << "Please Enter Account Number? ";
+	getline(cin >> ws, AccountNumber);
+
+	return AccountNumber;
+}
+
+bool FindClientByAccountNumber(string AccountNumber, vector <stClient> vClients, stClient& Client)
+{
+	for (stClient C : vClients)
+	{
+		if (C.AccountNumber == AccountNumber)
+		{
+			Client = C;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool MarkClientForDeleteByAccountNumber(string AccountNumber, vector <stClient>& vClients)
+{
+	for (stClient& C : vClients)
+	{
+		if (C.AccountNumber == AccountNumber)
+		{
+			C.MarkForDelete = true;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool DeleteClientByAccountNumber(string AccountNumber, vector <stClient>& vClients)
+{
+	stClient Client;
+	char Answer = 'n';
+
+	if (FindClientByAccountNumber(AccountNumber, vClients, Client))
+	{
+		PrintClientCard(Client);
+
+		cout << "\n\nAre you sure you want delete this client? Y/N ? ";
+		cin >> Answer;
+
+		if (toupper(Answer) == 'Y')
+		{
+			MarkClientForDeleteByAccountNumber(AccountNumber, vClients);
+			SaveCleintsDataToFile(ClientsFileName, vClients);
+
+			vClients = LoadCleintsDataFromFile(ClientsFileName);
+
+			cout << "\n\nClient Deleted Seuccessfully.\n";
+		}
+
+		return true;
+	}
 	else
-		return enWinner::Draw;
-}
-
-stGameResults FillGameResult(short GameRounds, short Player1WinTimes, short ComputerWinTimes, short DrawTimes)
-{
-	stGameResults GameResults;
-
-	GameResults.GameRounds = GameRounds;
-	GameResults.Player1WonTimes = Player1WinTimes;
-	GameResults.ComputerWonTimes = ComputerWinTimes;
-	GameResults.DrawTimes = DrawTimes;
-	GameResults.GameWinner = HowWonTheGame(Player1WinTimes, ComputerWinTimes);
-	GameResults.WinnerName = WinnerName(GameResults.GameWinner);
-
-	return GameResults;
-}
-
-enGameChoice ReadPlayer1Choice()
-{
-	short Choice = 1;
-
-	do
 	{
-		cout << "\nYour Choice: [1]:Stone, [2]:Paper, [3]:Scissors ? ";
-		cin >> Choice;
+		cout << "\nClient with Account Number (" << AccountNumber << ") Not Found!\n";
 
-	} while (Choice < 1 || Choice > 3);
-
-	return (enGameChoice)Choice;
+		return false;
+	}
 }
 
-enGameChoice GetComputer1Choice()
+stClient ChangeClientRecord(string AccountNumber)
 {
-	return (enGameChoice)RandomNumber(1, 3);
+	stClient Client;
+
+	Client.AccountNumber = AccountNumber;
+
+	cout << "\n\nEnter Pin Code? ";
+	getline(cin >> ws, Client.PinCode);
+
+	cout << "Enter Name? ";
+	getline(cin, Client.Name);
+
+	cout << "Enter Phone? ";
+	getline(cin, Client.Phone);
+
+	cout << "Enter Account Balance? ";
+	cin >> Client.AccountBalance;
+
+	return Client;
 }
 
-stGameResults PlayGame(short HowManyRounds)
+bool UpdateClientByAccountNumber(string AccountNumber, vector <stClient>& vClients)
 {
-	stRoundInfo RoundInfo;
-	short Player1WinTimes = 0, ComputerWinTimes = 0, DrawTimes = 0;
+	stClient Client;
+	char Answer = 'n';
 
-	for (short GameRound = 1; GameRound <= HowManyRounds; GameRound++)
+	if (FindClientByAccountNumber(AccountNumber, vClients, Client))
 	{
-		cout << "\nRound [" << GameRound << "] begins:\n";
-		RoundInfo.RoundNumber = GameRound;
-		RoundInfo.Player1Choice = ReadPlayer1Choice();
-		RoundInfo.Computer1Choice = GetComputer1Choice();
-		RoundInfo.Winner = HowWonTheRound(RoundInfo);
-		RoundInfo.WinnerName = WinnerName(RoundInfo.Winner);
+		PrintClientCard(Client);
 
-		if (RoundInfo.Winner == enWinner::Player1)
-			Player1WinTimes++;
-		else if (RoundInfo.Winner == enWinner::Computer)
-			ComputerWinTimes++;
-		else
-			DrawTimes++;
+		cout << "\n\nAre you sure you want update this client? y/n ? ";
+		cin >> Answer;
 
-		PrintRoundResults(RoundInfo);
+		if (toupper(Answer) == 'Y')
+		{
+			for (stClient& C : vClients)
+			{
+				if (C.AccountNumber == AccountNumber)
+				{
+					C = ChangeClientRecord(AccountNumber);
+					break;
+				}
+			}
+
+			SaveCleintsDataToFile(ClientsFileName, vClients, AccountNumber, Client);
+
+			cout << "\n\nClient Updated Seuccessfully.\n";
+		}
+
+		return true;
+	}
+	else
+	{
+		cout << "\nClient with Account Number (" << AccountNumber << ") Not Found!\n";
+
+		return false;
+	}
+}
+
+void ShowAllClientsScreen()
+{
+	vector <stClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+
+	cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client (s).";
+	cout << "\n________________________________________________________________________________________________\n" << endl;
+	cout << "| " << left << setw(15) << "Account Number";
+	cout << "| " << left << setw(10) << "Pin Code";
+	cout << "| " << left << setw(40) << "Client Name";
+	cout << "| " << left << setw(12) << "Phone";
+	cout << "| " << left << setw(12) << "Balance";
+	cout << "\n________________________________________________________________________________________________\n" << endl;
+
+	for (stClient& Client : vClients)
+	{
+		PrintClientRecord(Client);
+
+		cout << endl;
 	}
 
-	return FillGameResult(HowManyRounds, Player1WinTimes, ComputerWinTimes, DrawTimes);
+	cout << "________________________________________________________________________________________________\n" << endl;
 }
 
-string Tabs(short NumberOfTabs)
+void ShowAddNewClientScreen()
 {
-	string t = "";
+	cout << "\n-----------------------------------\n";
+	cout << "\tAdd New Clients Screen\n";
+	cout << "-----------------------------------\n";
 
-	for (int i = 1; i < NumberOfTabs; i++)
+	AddClients();
+}
+
+void ShowDeleteClientScreen()
+{
+	cout << "\n-----------------------------------\n";
+	cout << "\tDelete Client Screen\n";
+	cout << "-----------------------------------\n\n";
+
+	vector <stClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+	string AccountNumber = ReadClientAccountNumber();
+
+	DeleteClientByAccountNumber(AccountNumber, vClients);
+}
+
+void ShowUpdateClientInfoScreen()
+{
+	cout << "\n-----------------------------------\n";
+	cout << "\tUpdate Client Info Screen\n";
+	cout << "-----------------------------------\n\n";
+
+	vector <stClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+	string AccountNumber = ReadClientAccountNumber();
+
+	UpdateClientByAccountNumber(AccountNumber, vClients);
+}
+
+void ShowFindClientScreen()
+{
+	cout << "\n-----------------------------------\n";
+	cout << "\tFind Client Screen\n";
+	cout << "-----------------------------------\n\n";
+
+	vector <stClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+	stClient Client;
+	string AccountNumber = ReadClientAccountNumber();
+
+	if (FindClientByAccountNumber(AccountNumber, vClients, Client))
 	{
-		t += "\t";
-		cout << t;
+		PrintClientCard(Client);
 	}
-
-	return t;
-}
-
-void ShowGameOverScreen()
-{
-	cout << Tabs(2) << "__________________________________________________________\n\n";
-	cout << Tabs(2) << "                 +++ G a m e  O v e r +++\n";
-	cout << Tabs(2) << "__________________________________________________________\n\n";
-}
-
-void ShowFinalGameResults(stGameResults GameResults)
-{
-	cout << Tabs(2) << "_____________________ [Game Results ]_____________________\n\n";
-	cout << Tabs(2) << "Game Rounds        : " << GameResults.GameRounds << endl;
-	cout << Tabs(2) << "Player1 won times  : " << GameResults.Player1WonTimes << endl;
-	cout << Tabs(2) << "Computer won times : " << GameResults.ComputerWonTimes << endl;
-	cout << Tabs(2) << "Draw times         : " << GameResults.DrawTimes << endl;
-	cout << Tabs(2) << "Final Winner       : " << GameResults.WinnerName << endl;
-	cout << Tabs(2) << "__________________________________________________________\n";
-
-	SetWinnerScreenColor(GameResults.GameWinner);
-}
-
-short ReadHowManyRounds()
-{
-	short GameRounds = 1;
-
-	do
+	else
 	{
-		cout << "How Many Rounds 1 to 10 ?\n";
-		cin >> GameRounds;
-
-	} while (GameRounds < 1 || GameRounds > 10);
-
-	return GameRounds;
+		cout << "\nClient with Account Number[" << AccountNumber << "] is not found!\n";
+	}
 }
 
-void ResetScreen()
+void ShowEndScreen()
+{
+	cout << "\n-----------------------------------\n";
+	cout << "\tProgram Ends :-)\n";
+	cout << "-----------------------------------\n";
+}
+
+void GoBackToMainMeune()
+{
+	cout << "\n\nPress any key to go back to Main Menue...";
+
+	system("pause>0");
+
+	ShowMainMenue();
+}
+
+enum enMainMunueOptions
+{
+	eClientsList = 1, eAddNewClients = 2, eDeleteClient = 3, eUpdateClient = 4, eFindClient = 5, eExit = 6
+};
+
+void PerfromMainMunueOptions(enMainMunueOptions MainMunueOption)
+{
+	switch (MainMunueOption)
+	{
+	case enMainMunueOptions::eClientsList:
+		system("cls");
+		ShowAllClientsScreen();
+		GoBackToMainMeune();
+		break;
+
+	case enMainMunueOptions::eAddNewClients:
+		system("cls");
+		ShowAddNewClientScreen();
+		GoBackToMainMeune();
+		break;
+
+	case enMainMunueOptions::eDeleteClient:
+		system("cls");
+		ShowDeleteClientScreen();
+		GoBackToMainMeune();
+		break;
+
+	case enMainMunueOptions::eUpdateClient:
+		system("cls");
+		ShowUpdateClientInfoScreen();
+		GoBackToMainMeune();
+		break;
+
+	case enMainMunueOptions::eFindClient:
+		system("cls");
+		ShowFindClientScreen();
+		GoBackToMainMeune();
+		break;
+
+	case enMainMunueOptions::eExit:
+		system("cls");
+		ShowEndScreen();
+		break;
+	}
+}
+
+short ReadMainMunueOption()
+{
+	short Choice = 0;
+
+	cout << "Choose what do you want to do? [1 to 6]? ";
+	cin >> Choice;
+
+	return Choice;
+}
+
+void ShowMainMenue()
 {
 	system("cls");
-	system("color 0F");
-}
 
-void StartGame()
-{
-	char PlayAgain = 'Y';
+	cout << "===========================================\n";
+	cout << "\t\tMain Menue Screen\n";
+	cout << "===========================================\n";
+	cout << "\t[1] Show Client List.\n";
+	cout << "\t[2] Add New Client.\n";
+	cout << "\t[3] Delete Client.\n";
+	cout << "\t[4] Update Client Info.\n";
+	cout << "\t[5] Find Client.\n";
+	cout << "\t[6] Exit.\n";
+	cout << "===========================================\n";
 
-	do
-	{
-		ResetScreen();
-		stGameResults GameResults = PlayGame(ReadHowManyRounds());
-		ShowGameOverScreen();
-		ShowFinalGameResults(GameResults);
-
-		cout << endl << Tabs(2) << "Do you want to play again? Y/N? ";
-		cin >> PlayAgain;
-
-	} while (PlayAgain == 'Y' || PlayAgain == 'y');
+	PerfromMainMunueOptions(enMainMunueOptions(ReadMainMunueOption()));
 }
 
 int main()
 {
-	srand((unsigned)time(NULL));
+	ShowMainMenue();
 
-	StartGame();
+	system("pause>0");
 
 	return 0;
 }
